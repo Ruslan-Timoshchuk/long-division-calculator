cda8a70b76a685f0e1d53ee1e5650c3211a6cff6
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2025-12-09 10:16:36 +0100

    implemented calculator

---
+++ src/main/java/com/calculator/longdivision/DivisionStep.java
@@ -0,0 +1,20 @@
+package com.calculator.longdivision;
+
+public class DivisionStep {
+
+	private final int minuend;
+	private final int subtrahend;
+
+	public DivisionStep(int minuend, int subtrahend) {
+		this.minuend = minuend;
+		this.subtrahend = subtrahend;
+	}
+
+	public int getMinuend() {
+		return minuend;
+	}
+
+	public int getSubtrahend() {
+		return subtrahend;
+	}
+}
