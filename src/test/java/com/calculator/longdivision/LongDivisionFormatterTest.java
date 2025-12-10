030a50516621bada4f1c8a89e915c46fb5b109a4
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2025-12-10 19:30:16 +0100

    added tests for long division formatter

---
+++ src/test/java/com/calculator/longdivision/LongDivisionFormatterTest.java
@@ -0,0 +1,13 @@
+package com.calculator.longdivision;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class LongDivisionFormatterTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
