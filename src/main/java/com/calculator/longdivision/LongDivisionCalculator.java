179913ace6f5073faab307bf13cf99dc87bbc871
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2025-12-10 12:18:10 +0100

    improved CLI menu

--- src/main/java/com/calculator/longdivision/LongDivisionCalculator.java
+++ src/main/java/com/calculator/longdivision/LongDivisionCalculator.java
@@ -5,21 +5,33 @@ import java.util.Scanner;
 public class LongDivisionCalculator {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter dividend and divisor: ");
-        int dividend = requireInput(scanner);
-        int divisor = requireInput(scanner);
-        scanner.close();
         LongDivisionProcessor processor = new LongDivisionProcessor();
-        LongDivisionResult result = processor.divide(dividend, divisor);
         LongDivisionFormatter formatter = new LongDivisionFormatter();
-        System.out.println(formatter.format(result));
+        System.out.println("Welcome to the long division calculator app!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("Enter dividend and divisor: ");
+                int dividend = requireInput(scanner);
+                int divisor = requireInput(scanner);
+                LongDivisionResult result = processor.divide(dividend, divisor);
+                System.out.println(formatter.format(result));
+                System.out.println("Exit? Yes / No");
+                String userChoice = scanner.next();
+                if (userChoice.equalsIgnoreCase("yes")) {
+                    System.out.println("See you later!)");
+                    break;
+                } else {
+                    System.out.println(
+                            "Continuing the program... you entered: '" + userChoice + "'. Let's move forward!");
+                }
+            }
+        }
     }
 
     private static int requireInput(Scanner scanner) {
         while (!scanner.hasNextInt()) {
-            System.out.println("The " + scanner.next()
-                    + " is incorrect input! Entered must be a number and not more then 2147483647");
+            System.out
+                    .println("Invalid input: '" + scanner.next() + "'. Please enter a valid integer (max 2147483647)!");
         }
         return scanner.nextInt();
     }
