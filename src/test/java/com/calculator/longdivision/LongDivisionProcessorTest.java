0b15fbcc8cc383394a6e2a02a36318c92e3ddf03
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2025-12-10 15:35:57 +0100

    added tests for the calculator

---
+++ src/test/java/com/calculator/longdivision/LongDivisionProcessorTest.java
@@ -0,0 +1,13 @@
+package com.calculator.longdivision;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class LongDivisionProcessorTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
